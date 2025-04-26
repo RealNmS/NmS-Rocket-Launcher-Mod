a985608322007216ee6965570d32fb323cd23c3c
RealNmS <sovicka.03@seznam.cz>
2025-04-26 17:37:23 +0200

    Remove unused code

--- src/main/java/com/nms/nmsrocketlaunchermod/client/RocketLauncherModClient.java
+++ src/main/java/com/nms/nmsrocketlaunchermod/client/RocketLauncherModClient.java
@@ -2,13 +2,13 @@ package com.nms.nmsrocketlaunchermod.client;
 
 import com.nms.nmsrocketlaunchermod.client.render.RocketEntityRenderer;
 import com.nms.nmsrocketlaunchermod.entity.ModEntities;
-
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
 public class RocketLauncherModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        // Register the rocket entity renderer
         EntityRendererRegistry.register(ModEntities.ROCKET, RocketEntityRenderer::new);
     }
 }
