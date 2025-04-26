270a2ba86be6123b4b3b97cff5093b39b4cb3284
RealNmS <sovicka.03@seznam.cz>
2025-04-26 18:13:35 +0200

    Add root advancement

--- src/main/java/com/nms/nmsrocketlaunchermod/client/RocketLauncherModClient.java
+++ src/main/java/com/nms/nmsrocketlaunchermod/client/RocketLauncherModClient.java
@@ -2,6 +2,7 @@ package com.nms.nmsrocketlaunchermod.client;
 
 import com.nms.nmsrocketlaunchermod.client.render.RocketEntityRenderer;
 import com.nms.nmsrocketlaunchermod.entity.ModEntities;
+
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 
