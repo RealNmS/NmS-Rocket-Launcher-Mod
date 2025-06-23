81e53247b6e70504e07f25e6222406bb20d48346
RealNmS <sovicka.03@seznam.cz>
2025-06-23 21:44:54 +0200

    Change Rocket Launcher item group

--- src/client/java/com/nms/nmsrocketlaunchermod/item/ModItems.java
+++ src/client/java/com/nms/nmsrocketlaunchermod/item/ModItems.java
@@ -2,28 +2,23 @@ package com.nms.nmsrocketlaunchermod.item;
 
 import com.nms.nmsrocketlaunchermod.RocketLauncherMod;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
 
 public class ModItems {
     public static final Item ROCKET_LAUNCHER = new RocketLauncherItem(
             new Item.Settings().maxCount(1));
 
-    public static final ItemGroup ROCKET_GROUP = FabricItemGroup.builder(
-            new Identifier(RocketLauncherMod.MOD_ID, "rocket_group"))
-            .icon(() -> new ItemStack(ROCKET_LAUNCHER))
-            .displayName(Text.translatable("itemGroup.nmsrocketlaunchermod"))
-            .entries((displayContext, entries) -> entries.add(ROCKET_LAUNCHER))
-            .build();
-
     public static void registerItems() {
         Registry.register(Registries.ITEM, new Identifier(RocketLauncherMod.MOD_ID, "rocket_launcher"),
                 ROCKET_LAUNCHER);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register((entries) -> {
+            entries.add(ROCKET_LAUNCHER);
+        });
     }
 }
