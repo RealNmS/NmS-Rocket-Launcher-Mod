af3064765773e5773b861558b7d4bd54d3f1728a
RealNmS <sovicka.03@seznam.cz>
2025-06-23 21:38:59 +0200

    Fix indent

--- src/client/java/com/nms/nmsrocketlaunchermod/item/ModItems.java
+++ src/client/java/com/nms/nmsrocketlaunchermod/item/ModItems.java
@@ -12,18 +12,18 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 
 public class ModItems {
-        public static final Item ROCKET_LAUNCHER = new RocketLauncherItem(
-                        new Item.Settings().maxCount(1));
+    public static final Item ROCKET_LAUNCHER = new RocketLauncherItem(
+            new Item.Settings().maxCount(1));
 
-        public static final ItemGroup ROCKET_GROUP = FabricItemGroup.builder(
-                        new Identifier(RocketLauncherMod.MOD_ID, "rocket_group"))
-                        .icon(() -> new ItemStack(ROCKET_LAUNCHER))
-                        .displayName(Text.translatable("itemGroup.nmsrocketlaunchermod"))
-                        .entries((displayContext, entries) -> entries.add(ROCKET_LAUNCHER))
-                        .build();
+    public static final ItemGroup ROCKET_GROUP = FabricItemGroup.builder(
+            new Identifier(RocketLauncherMod.MOD_ID, "rocket_group"))
+            .icon(() -> new ItemStack(ROCKET_LAUNCHER))
+            .displayName(Text.translatable("itemGroup.nmsrocketlaunchermod"))
+            .entries((displayContext, entries) -> entries.add(ROCKET_LAUNCHER))
+            .build();
 
-        public static void registerItems() {
-                Registry.register(Registries.ITEM, new Identifier(RocketLauncherMod.MOD_ID, "rocket_launcher"),
-                                ROCKET_LAUNCHER);
-        }
+    public static void registerItems() {
+        Registry.register(Registries.ITEM, new Identifier(RocketLauncherMod.MOD_ID, "rocket_launcher"),
+                ROCKET_LAUNCHER);
+    }
 }
